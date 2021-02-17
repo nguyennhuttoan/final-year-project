95f2012d9b12ef694852b9397cdb649c11caa11a
nguyennhuttoan <toannhutnguyen@gmail.com>
2021-02-17 23:28:52 +0700

    add: first route

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const products = require('./routes/product');
+
 const app = express();
+
+app.use(express.json());
+app.use('/api/v1', products);
 
 module.exports = app;
