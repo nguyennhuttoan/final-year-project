2e7756051da15a8279b73f638ec5fefa1d4e1345
nguyennhuttoan <toannhutnguyen@gmail.com>
2021-02-19 15:54:37 +0700

    add: get single product api

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -17,3 +17,19 @@ exports.getProducts = async (req, res, next) => {
     products,
   });
 };
+
+exports.getSingleProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: 'Prduct not found',
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { getProducts, newProduct } = require('../controllers/ProductController');
+const { getProducts, newProduct, getSingleProduct } = require('../controllers/ProductController');
 
 const router = express.Router();
 
 router.route('/products').get(getProducts);
+router.route('/product/:id').get(getSingleProduct);
 
 router.route('/product/new').post(newProduct);
+
+
 
 module.exports = router;
