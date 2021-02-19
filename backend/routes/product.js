ce9a9350701da2d341f249943544b83829e10402
nguyennhuttoan <toannhutnguyen@gmail.com>
2021-02-19 16:15:04 +0700

    add: update product api

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const { getProducts, newProduct, getSingleProduct } = require('../controllers/ProductController');
+const {
+  getProducts,
+  newProduct,
+  getSingleProduct,
+  updateProduct,
+} = require('../controllers/ProductController');
 
 const router = express.Router();
 
@@ -7,7 +12,6 @@ router.route('/products').get(getProducts);
 router.route('/product/:id').get(getSingleProduct);
 
 router.route('/product/new').post(newProduct);
-
-
+router.route('/admin/product/:id').post(updateProduct);
 
 module.exports = router;
