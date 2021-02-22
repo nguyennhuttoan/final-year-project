20da87ff36ca315f5b40caf3cc25482ec991bae9
nguyennhuttoan <toannhutnguyen@gmail.com>
2021-02-22 19:19:08 +0700

    add: error handler

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const products = require('./routes/product');
+const errMiddleware = require('./middlewares/errors');
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/v1', products);
+app.use(errMiddleware);
 
 module.exports = app;
